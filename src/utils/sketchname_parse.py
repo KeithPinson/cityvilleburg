282ef6f17844a4bb3d3ccd6ed3ec281e6f13b4da
Keith <me@keithpinson.com>
2021-02-22 11:08:43 -0500

    Incorporated possible import names

--- src/utils/sketchname_parse.py
+++ src/utils/sketchname_parse.py
@@ -29,73 +29,229 @@
 import re
 
 
-# pylint: disable=too-many-arguments
+# pylint: disable=too-many-arguments, too-many-instance-attributes, too-many-locals
+
+def build_sketchname_string(city, seed, style, x, y, tile="", variant=0, import_name=""):
+    # pylint: disable=invalid-name, too-many-locals
+    """From the parameters build and return the Sketch Name string"""
+
+    sketch_name = ""
+
+    # Make sure one set of parameters are set or the other (import name)
+    if not verify_sketchname_parameters(city, seed, style, x, y, import_name):
+        return sketch_name
+
+    #
+    # Import Name
+    #
+    if len(import_name) > 0:
+
+        # We are going to enforce the same rules for file names
+        t_name = format_file_name(import_name)
+
+        sketch_name = """{name}""".format(name=t_name)
+
+        return sketch_name
+
+    # The tile needs to either be an empty string or a zero-filled,
+    # five digit, numeric string preceded by a dash (-)
+    t_tile = ""
+
+    if tile != "":
+        try:
+            v = int(tile.lstrip('-'))
+            t_tile = "-" + str(v).zfill(5)
+        except ValueError:
+            t_tile = ""
+        else:
+            t_tile = ""
+
+    # Variant needs to either be an empty string or a zero-filled,
+    # three digit, numeric string preceded by a dot (.)
+    t_variant = ""
+
+    try:
+        v = int(variant)
+
+        if v > 0:
+            t_variant = "." + str(v).zfill(3)
+
+    except ValueError:
+        t_variant = ""
+
+    # If subtype='FILE_NAME' was used with the StringProperty we should be
+    # passed a filename compatible string, however we still may need to remove
+    # quoting and spaces, so we will format the string making no assumptions
+    t_city = format_city_name(city)
+
+    # Seeds should be an integer greater than zero if set
+    try:
+        t_seed = str(int(seed) if int(seed) > 0 else "")
+    except ValueError:
+        t_seed = ""
+
+    # We only use the first letter
+    try:
+        t_style = style[0]
+    except IndexError:
+        t_style = ""
+
+    # X and Y size follow the same rules: Basically convert to km or indicate meters
+    t_x = convert_length_to_km_string(x)
+    t_y = convert_length_to_km_string(y)
+
+    # Validate the strings before building the record
+    if(len(t_city) > 0 and
+       len(t_seed) > 0 and
+       len(t_style) > 0 and
+       len(t_x) > 0 and
+       len(t_y) > 0):
+
+        # We'll add a # character if the city name ends in a number
+        last_letter = t_city[-1:]
+        if last_letter.isnumeric():
+            t_city = t_city + "#"
+
+        # Example: city1_g1x1
+        sketch_name = """{city}{seed}_{style}{x}x{y}{tile}{variant}""". \
+            format(
+                city=t_city,
+                seed=t_seed,
+                style=t_style,
+                x=t_x,
+                y=t_y,
+                tile=t_tile,
+                variant=t_variant)
+
+    return sketch_name
+
+
+def convert_length_to_km_string(length):
+    """Convert length like 1000 to '1' or 30 to '30M'"""
+
+    try:
+        t_length = int(length)
+
+        if t_length < 1000:
+            length_str = str(t_length) + "M"
+        else:
+            length_str = str(int(t_length / 1000))
+
+    except ValueError:
+        length_str = ""
+    else:
+        length_str = ""
+
+    return length_str
+
+
+def format_city_name(city):
+    # pylint: disable=line-too-long
+    """Format the city name string for incorporating into Sketch Name string"""
+    #
+    # The string must:
+    #
+    #   1) Be filename compatible
+    #   2) Must not contain an underscore, "_"
+    #   3) Each word must be capitalized
+    #   4) All space must be removed
+    #   5) Not be quoted
+    #
+
+    try:
+        t_city = city
+
+        # Remove leading/following quotes
+        t_city = t_city.strip("""\"""")
+        t_city = t_city.strip("""\'""")
+
+        # Remove <>:"/\|?* and _
+        t_city = format_file_name(t_city)
+        t_city = t_city.replace("_", "")    # Underscore is reserved as a delimiter in the sketchname
+
+        # Capitalize words and remove spaces
+        t_city = t_city.title()  # Underscores have to be removed before calling this
+        t_city = t_city.replace(" ", "")
+
+    except TypeError:
+        t_city = ""
+    else:
+        t_city = ""
+
+    return t_city
+
+
+def format_file_name(file_name):
+    """Format the string so that it is valid file format"""
+
+    try:
+        t_file_name = file_name
+
+        # Remove <>:"/\|?* and _
+        t_file_name = re.sub(r"[<>:\"/\\|?*]", "", t_file_name)
+
+    except TypeError:
+        t_file_name = ""
+    else:
+        t_file_name = ""
+
+    return t_file_name
+
+
+def verify_sketchname_parameters(city, seed, style, x, y, import_name):
+    """Verify that we are not trying to set import name and other parameters"""
+    # pylint: disable=invalid-name
+
+    is_valid = True
+
+    if not isinstance(city, str) or len(city) == 0:
+        is_valid = False
+
+    if not str(seed).isnumeric():
+        is_valid = False
+
+    if not isinstance(style, str) or len(style) == 0:
+        is_valid = False
+
+    if not str(x).isnumeric():
+        is_valid = False
+
+    if not str(y).isnumeric():
+        is_valid = False
+
+    # We are okay if everything is invalid but we have an import_name
+    if not is_valid and (isinstance(import_name, str) and len(import_name) > 0):
+        is_valid = True
+
+    return is_valid
+
 
 class SketchName:
+    """Unless imported, Sketches are identified by a combination of parameters"""
 
     sketch_name = ""
     city = ""
+    imported = False
     seed = ""
     style = ""
     x = ""
     y = ""
     tile = ""
     variant = 0
+    import_name = ""
 
-    def __init__(self, city="", seed="", style="", x="", y="", tile="", variant=0):
+    def __init__(
+            self,
+            city="",
+            seed="",
+            style="",
+            x="",
+            y="",
+            tile="",
+            variant=0,
+            import_name=""):
 
-        self.update_sketchname(city, seed, style, x, y, tile, variant)
-
-    def format_city_name(self, city):
-        """Format the city name string for incorporating into Sketch Name string"""
-        #
-        # The string must:
-        #
-        #   1) Be filename compatible
-        #   2) Must not contain an underscore, "_"
-        #   3) Each word must be capitalized
-        #   4) All space must be removed
-        #   5) Not be quoted
-        #
-
-        try:
-            t_city = city
-
-            # Remove leading/following quotes
-            t_city = t_city.strip("""\"""")
-            t_city = t_city.strip("""\'""")
-
-            # Remove <>:"/\|?* and _
-            t_city = re.sub(r"[<>:\"/\\\|?*]", "", t_city)
-
-            # Capitalize words and remove spaces
-            t_city = t_city.title()  # Underscores have to be removed before calling this
-            t_city = t_city.replace(" ", "")
-
-        except TypeError:
-            t_city = ""
-        else:
-            t_city = ""
-
-        return t_city
-
-    def convert_length_to_km_string(self, length):
-        """Convert length like 1000 to '1' or 30 to '30M'"""
-
-        try:
-            t_length = int(length)
-
-            if t_length < 1000:
-                length_str = str(t_length) + "M"
-            else:
-                length_str = str(int(t_length / 1000))
-
-        except ValueError:
-            length_str = ""
-        else:
-            length_str = ""
-
-        return length_str
+        self.update_sketchname(city, seed, style, x, y, tile, variant, import_name)
 
     def string_to_sketchname(self, sketchname_string):
         # pylint: disable=line-too-long, invalid-name
@@ -133,7 +289,7 @@ class SketchName:
         m = re.match(rex, sketchname_string)
 
         if m is not None:
-            city, seed, style, x, y, tile, variant = m.groups()
+            city, seed, style, x, y, tile, variant, import_name = m.groups()
 
             self.sketch_name = sketchname_string
             self.city = city
@@ -143,90 +299,25 @@ class SketchName:
             self.y = y
             self.tile = tile if tile is not None else ""
             self.variant = variant if variant is not None else 0
+            self.import_name = import_name if import_name is not None else ""
 
     def sketchname_string(self):
         """Extract the Sketch Name string and return it"""
 
         return self.sketch_name
 
-    def build_sketchname_string(self, city, seed, style, x, y, tile="", variant=0):
-        # pylint: disable=invalid-name, too-many-locals
-        """From the parameters build and return the Sketch Name string"""
+    def update_sketchname(
+            self,
+            city="",
+            seed="",
+            style="",
+            x="",
+            y="",
+            tile="",
+            variant=0,
+            import_name=""):
 
-        sketch_name = ""
-
-        # The tile needs to either be an empty string or a zero-filled,
-        # five digit, numeric string preceded by a dash (-)
-        t_tile = ""
-
-        if tile != "":
-            try:
-                v = int(tile.lstrip('-'))
-                t_tile = "-" + str(v).zfill(5)
-            except ValueError:
-                t_tile = ""
-
-        # Variant needs to either be an empty string or a zero-filled,
-        # three digit, numeric string preceded by a dot (.)
-        t_variant = ""
-
-        try:
-            v = int(variant)
-
-            if v > 0:
-                t_variant = "." + str(v).zfill(3)
-
-        except ValueError:
-            t_variant = ""
-
-        # If subtype='FILE_NAME' was used with the StringProperty we should be
-        # passed a filename compatible string, however we still may need to remove
-        # quoting and spaces, so we will format the string making no assumptions
-        t_city = self.format_city_name(city)
-
-        # Seeds should be an integer greater than zero if set
-        try:
-            t_seed = str(int(seed) if int(seed) > 0 else "")
-        except ValueError:
-            t_seed = ""
-
-        # We only use the first letter
-        try:
-            t_style = style[0]
-        except IndexError:
-            t_style = ""
-
-        # X and Y size follow the same rules: Basically convert to km or indicate meters
-        t_x = self.convert_length_to_km_string(x)
-        t_y = self.convert_length_to_km_string(y)
-
-        # Validate the strings before building the record
-        if(len(t_city) > 0 and
-           len(t_seed) > 0 and
-           len(t_style) > 0 and
-           len(t_x) > 0 and
-           len(t_y) > 0):
-
-            # We'll add a # character if the city name ends in a number
-            last_letter = t_city[-1:]
-            if last_letter.isnumeric():
-                t_city = t_city + "#"
-
-            # Example: city1_g1x1
-            sketch_name = """{city}{seed}_{style}{x}x{y}{tile}{variant}""". \
-                format(
-                    city=t_city,
-                    seed=t_seed,
-                    style=t_style,
-                    x=t_x,
-                    y=t_y,
-                    tile=t_tile,
-                    variant=t_variant)
-
-        return sketch_name
-
-    def update_sketchname(self, city="", seed="", style="", x="", y="", tile="", variant=0):
-        # pylint: invalid-name
+        # pylint: disable=invalid-name
         """Usage: update_sketchname( city="London" ) for example to change the city member"""
 
         #
@@ -244,24 +335,27 @@ class SketchName:
         t_y = y if y != "" else self.y
         t_tile = tile if tile != "" else self.tile
         t_variant = variant if variant != 0 else self.variant
+        t_import_name = import_name if import_name != "" else self.import_name
 
-        t_sketch_name = self.build_sketchname_string(
+        t_sketch_name = build_sketchname_string(
             t_city,
             t_seed,
             t_style,
             t_x,
             t_y,
             t_tile,
-            t_variant)
+            t_variant,
+            t_import_name)
 
-        self.sketch_name = t_sketch_name,
-        self.city = t_city,
-        self.seed = t_seed,
-        self.style = t_style,
-        self.x = t_x,
-        self.y = t_y,
-        self.tile = t_tile,
+        self.sketch_name = t_sketch_name
+        self.city = t_city
+        self.seed = t_seed
+        self.style = t_style
+        self.x = t_x
+        self.y = t_y
+        self.tile = t_tile
         self.variant = t_variant
+        self.import_name = t_import_name
 
         return t_sketch_name
 
