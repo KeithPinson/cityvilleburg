9af09602883759ae90f0e0458306ed5c6c98922a
Keith <me@keithpinson.com>
2021-06-25 13:02:23 -0400

    Missed commit, Pin max tile number

--- __init__.py
+++ __init__.py
@@ -16,16 +16,11 @@
 __doc__ = """A Blender addon for generating cities"""
 __name__ = "CITYVILLEBURG"
 
-
 from inspect import isclass
 from bpy.utils import register_class, unregister_class
 from .src.addon.preferences import cvb_addon_register, cvb_addon_unregister
 from .src.addon.preferences import CVB_AddonPreferences
-from .src.panel.n_key_panel import CVB_PT_Main, CVB_OT_NewMapButton, CVB_OT_GenCityButton
-from .src.panel.n_key_panel import CVB_PT_Help, CVB_OT_GettingStartedHelp
-from .src.panel.panel_ops import CVB_OT_NewSketchButton
-from .src.panel.panel_props import cvb_panel_register, cvb_panel_unregister
-
+# Hold off on importing the modules with properties
 
 bl_info = {
     "name": "CITYVILLEBURG",
@@ -42,9 +37,21 @@ bl_info = {
     "tracker_url": "https://github.com/KeithPinson/cityvilleburg/issues"
 }
 
-
 # Make it clear in the console output where the output of this addon begins
 print("\n", f'''*** {bl_info['name']} ***''')
+
+
+# The addon class may contain data referenced by the
+# other classes so we make sure it is pre-registered
+register_class(CVB_AddonPreferences)
+
+
+# Now that the addon preferences are loaded, import the modules with properties
+from .src.panel.n_key_panel import CVB_PT_Main, CVB_OT_NewMapButton, CVB_OT_GenCityButton
+from .src.panel.n_key_panel import CVB_PT_Help, CVB_OT_GettingStartedHelp
+from .src.panel.panel_ops import CVB_OT_NewSketchButton
+from .src.panel.panel_props import cvb_panel_register, cvb_panel_unregister
+
 
 # Ideally we should declare and define the hooks for the Blender
 # classes in their respective packages, but because of the ease at
@@ -60,7 +67,6 @@ _CLASS_REGISTRY = (
     CVB_PT_Main,
     CVB_PT_Help,
 )
-
 
 def verify_classes(registry):
     """Verify that we have all our required classes"""
@@ -78,17 +84,13 @@ _CLASSES_VERIFIED = verify_classes(_CLASS_REGISTRY)
 
 
 def register():
-    """Register the addon"""
-
-    # The addon classes may contain data referenced by the
-    # other classes so we make sure they are registered first
-    register_class(CVB_AddonPreferences)
+    """Register the rest of the addon"""
 
     # Setup any additional data the other classes may need
     cvb_addon_register()
     cvb_panel_register()
 
-    # Okay, load the remainder of the classes
+    # Okay, load the remainder of the classes (skip what we pre-registered)
     for cls in _CLASS_REGISTRY[1:]:
         register_class(cls)
 
